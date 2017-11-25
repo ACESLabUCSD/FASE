b510e315c6f985f366b455c186ffac2302bebf79
Siam Umar Hussain <siamumar@ucsd.edu>
2017-11-25 02:32:55 -0800

    renames AES128.v to AES_128.v

--- Vivado/AES/AES.tcl
+++ Vivado/AES/AES.tcl
@@ -32,7 +32,7 @@
 #    "C:/Users/Siam/git/MAXelerator/AES/MixColumns.sv"
 #    "C:/Users/Siam/git/MAXelerator/AES/ShiftRows.sv"
 #    "C:/Users/Siam/git/MAXelerator/AES/SubBytes.sv"
-#    "C:/Users/Siam/git/MAXelerator/AES/AES128.sv"
+#    "C:/Users/Siam/git/MAXelerator/AES/AES_128.sv"
 #    "C:/Users/Siam/git/MAXelerator/testbenches/tb_AES.sv"
 #
 #*****************************************************************************************
@@ -135,7 +135,7 @@ set files [list \
  "[file normalize "$origin_dir/../../AES/MixColumns.sv"]"\
  "[file normalize "$origin_dir/../../AES/ShiftRows.sv"]"\
  "[file normalize "$origin_dir/../../AES/SubBytes.sv"]"\
- "[file normalize "$origin_dir/../../AES/AES128.sv"]"\
+ "[file normalize "$origin_dir/../../AES/AES_128.sv"]"\
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -165,7 +165,7 @@ set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
-set file "$origin_dir/../../AES/AES128.sv"
+set file "$origin_dir/../../AES/AES_128.sv"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
