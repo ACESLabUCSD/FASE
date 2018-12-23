35cad78af40e47d13835b217f73e801b3f279eb5
siamumar <siamumar@ucsd.edu>
2018-12-22 19:15:28 -0800

    read input indices from file in GC testbench

--- testbenches/tb_GC_engine.sv
+++ testbenches/tb_GC_engine.sv
@@ -41,22 +41,30 @@ module tb_GC_engine;
 	
 	parameter LOC = "/home/siam/git/hostCPU_TG/hw_aclrtr/all_logic/";
 	parameter KEYFILE = "Keys.txt";
+	parameter IN0FILE = "In0.txt";
+	parameter IN1FILE = "In1.txt";
 	parameter LOGICFILE = "Logics.txt";
 	parameter LABELFILE = "Labels.txt";
 	parameter TABLEFILE = "Tables.txt";
 	parameter OLABELFILE = "OLabels.txt";
 	
 	logic	[K-1:0]	KEYS [0:1];
-	logic	[K-1:0]	IN_LABELS [0:17]; //The first two labels from the Labels file are assigned to constants
+	logic	[3:0]	IN0 [0:7]; 
+	logic	[3:0]	IN1 [0:7]; 
 	logic	[3:0]	G_LOGICS [0:7]; 
+	logic	[K-1:0]	IN_LABELS [0:17]; //The first two labels from the Labels file are assigned to constants
 	logic	[K-1:0]	T_REF [0:15];
 	logic	[K-1:0]	OUT_LABELS_REF [0:15];
+	
+	logic	[L-1:0]	in0, in1;
 	
 	integer i;
 
 	initial
 	begin
 		$readmemh({LOC, KEYFILE}, KEYS);
+		$readmemh({LOC, IN0FILE}, IN0);
+		$readmemh({LOC, IN1FILE}, IN1);
 		$readmemh({LOC, LOGICFILE}, G_LOGICS);
 		$readmemh({LOC, LABELFILE}, IN_LABELS);
 		$readmemh({LOC, TABLEFILE}, T_REF);
@@ -67,15 +75,17 @@ module tb_GC_engine;
 		cid = 'd0;
 		
 		for (i = 0; i < 8; i = i+1) begin			
-			gid = i;		
+			gid = i;	
+			in0 = IN0[i] + 2; //The first two labels from the Labels file are assigned to constants
+			in1 = IN1[i] + 2;
 			g_logic = G_LOGICS[i];
-			in0_label = IN_LABELS[i+2]; //The first two labels from the Labels file are assigned to constants
-			in1_label = IN_LABELS[i+10];	
+			in0_label = IN_LABELS[in0];
+			in1_label = IN_LABELS[in1];	
 			t0_ref = T_REF[2*i];
 			t1_ref = T_REF[2*i+1];	
 			out_label_ref = OUT_LABELS_REF[2*i];
 			#1;
-			$display("out_label = %x \nt0 = %x \nt1 = %x", out_label, t0, t1);
+			$display("%d\nout_label = %x \nt0 = %x \nt1 = %x", i, out_label, t0, t1);
 			assert((out_label == out_label_ref) && (t0 == t0_ref) && (t1 == t1_ref)) $display("Pass");
 			else $error("Correct Values: \nout_label = %x \nt0 = %x \nt1 = %x", out_label_ref, t0_ref, t1_ref);			
 			#10;
