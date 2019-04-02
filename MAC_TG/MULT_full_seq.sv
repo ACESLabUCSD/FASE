29e032792fce6bf8d68547b43336eb449b4b022c
siamumar <siamumar@ucsd.edu>
2019-04-02 06:24:59 -0700

    X as init

--- MAC_TG/MULT_full_seq.sv
+++ MAC_TG/MULT_full_seq.sv
@@ -5,14 +5,14 @@ module MULT_full_seq_TG #(parameter M = 8)(
 	input clk,
 	input rst,
 	input [0:0] g_input,
-	input [M-1:0] e_input,
+	input [M-1:0] e_init,
 	output [0:0] o
 );
 
 	MULT_full_seq #(.M(M)) _MULT_full_seq(
 		.clk(clk), .rst(rst),
 		.a(g_input),
-		.X(e_input),
+		.X(e_init),
 		.aX(o)
 	);
 
@@ -25,12 +25,15 @@ module MULT_full_seq #(M = 8)(
 	output	[0:0] aX
 );
 
-	logic [M-1:0] A;
+	logic [M-1:0] A, X0;
 	
 	integer i;
 	
 	always@(posedge clk or posedge rst) begin
-		if(rst) A <= {M{1'b0}};
+		if(rst) begin
+			A <= {M{1'b0}};
+			X0 <= X;
+		end
 		else begin
 			A[0] <= a;
 			for(i = 0; i < M-1; i = i+1)
@@ -42,7 +45,7 @@ module MULT_full_seq #(M = 8)(
 	logic [M/2-1:0]CI[log2(M/2):0];
 	logic [M/2-1:0]CO[log2(M/2):0];
 	
-	assign S[0] = A&X;
+	assign S[0] = A&X0;
 		
 	always@(posedge clk or posedge rst) begin
 		if(rst) begin
